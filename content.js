870c4307a61cab538c2fb15a40019300f268059c
deanOfWalls <deanofwalls@gmail.com>
2023-09-04 05:27:39 -0400

    Changed to a different API and zip code converstion is now working

--- content.js
+++ content.js
@@ -1,39 +1,49 @@
 // Variable to store the state of the overlay
 let overlayVisible = false;
 
-// Function to show the UI overlay
 function showOverlay() {
   // Retrieve saved settings
-  chrome.storage.sync.get(['zipCode', 'country', 'timePosted', 'remote'], function (items) {
+  chrome.storage.sync.get(['country', 'timePosted', 'remote'], function (items) {
     const overlayHTML = `
-        <div id="overlay">
-          <label for="zipCode">Zip Code:</label>
-          <input type="text" id="zipCode" value="${items.zipCode || ''}"><br>
-          
-          <label for="country">Country:</label>
-          <select id="country">
-            <option value="USA">USA</option>
-            <option value="Canada">Canada</option>
-            <!-- Add more countries here -->
-          </select><br>
-          
-          <label for="timePosted">Time Posted:</label>
-          <select id="timePosted">
-            <option value="anytime">Any time</option>
-            <option value="pastMonth">Past Month</option>
-            <option value="pastWeek">Past Week</option>
-            <option value="past24Hours">Past 24 hours</option>
-          </select><br>
-          
-          <label for="remote">Remote:</label>
-          <select id="remote">
-            <option value="No">No</option>
-            <option value="Yes">Yes</option>
-          </select><br>
-          
-          <button id="searchButton">Search</button>
-        </div>
-      `;
+      <div id="overlay">
+        <label for="zipCode">Zip Code:</label>
+        <input type="text" id="zipCode" value=""><br>
+        
+        <label for="distance">Distance:</label>
+        <select id="distance">
+          <option value="none">None</option>
+          <option value="0">0 miles</option>
+          <option value="5">5 miles</option>
+          <option value="10">10 miles</option>
+          <option value="25">25 miles</option>
+          <option value="50">50 miles</option>
+          <option value="100">100 miles</option>
+        </select><br>
+        
+        <label for="country">Country:</label>
+        <select id="country">
+          <option value="USA">USA</option>
+          <option value="Canada">Canada</option>
+          <!-- Add more countries here -->
+        </select><br>
+        
+        <label for="timePosted">Time Posted:</label>
+        <select id="timePosted">
+          <option value="anytime">Any time</option>
+          <option value="pastMonth">Past Month</option>
+          <option value="pastWeek">Past Week</option>
+          <option value="past24Hours">Past 24 hours</option>
+        </select><br>
+        
+        <label for="remote">Remote:</label>
+        <select id="remote">
+          <option value="No">No</option>
+          <option value="Yes">Yes</option>
+        </select><br>
+        
+        <button id="searchButton">Search</button>
+      </div>
+    `;
     const overlay = document.createElement('div');
     overlay.innerHTML = overlayHTML;
     document.body.appendChild(overlay);
@@ -45,53 +55,95 @@ function showOverlay() {
 
     // Event listener for the "Search" button
     document.getElementById('searchButton').addEventListener('click', function () {
-      const zipCode = document.getElementById('zipCode').value;
       const country = document.getElementById('country').value;
       const timePosted = document.getElementById('timePosted').value;
       const remote = document.getElementById('remote').value;
+      const distance = document.getElementById('distance').value;
 
-      // Save settings
-      chrome.storage.sync.set({
-        zipCode: zipCode,
-        country: country,
-        timePosted: timePosted,
-        remote: remote
-      });
-
-      // Construct LinkedIn URL
-      const baseURL = "https://www.linkedin.com/jobs/search/?";
-      let query = `currentJobId=${getCurrentJobId()}`;
+      // Fetch location details using Zippopotamus HTTPS API
+      const zipCode = document.getElementById('zipCode').value;
 
       if (zipCode) {
-        query += `&location=${zipCode}`;
+        fetch(`https://api.zippopotam.us/us/${zipCode}`)
+          .then(response => response.json())
+          .then(data => {
+            const city = data.places[0]['place name'];
+            const state = data.places[0]['state'];
+
+            // Update the country to USA and fill city and state
+            document.getElementById('country').value = 'USA';
+            document.getElementById('zipCode').value = `${zipCode} ${city}, ${state}, United States`;
+
+            // Construct LinkedIn URL
+            const jobId = getCurrentJobId();
+            const location = `${zipCode}, ${city}, ${state}, United States`;
+            const baseURL = 'https://www.linkedin.com/jobs/search/?';
+            const queryParams = new URLSearchParams({
+              country: 'USA',
+              currentJobId: jobId,
+              geoId: 105328404,
+              location: location,
+              refresh: true
+            });
+
+            if (distance !== 'none') {
+              queryParams.append('distance', distance);
+            }
+
+            if (timePosted === 'past24Hours') {
+              queryParams.append('f_TPR', 'r86400');
+            } else if (timePosted === 'pastWeek') {
+              queryParams.append('f_TPR', 'r604800');
+            } else if (timePosted === 'pastMonth') {
+              queryParams.append('f_TPR', 'r2592000');
+            }
+
+            if (remote === 'Yes') {
+              queryParams.append('f_WT', '2');
+            }
+
+            const fullURL = `${baseURL}${queryParams.toString()}`;
+
+            // Navigate to the URL
+            window.location.href = fullURL;
+          })
+          .catch(error => {
+            console.error('An error occurred:', error);
+          });
+      } else {
+        // No zip code entered, use country as search term
+        const jobId = getCurrentJobId();
+        const baseURL = 'https://www.linkedin.com/jobs/search/?';
+        const queryParams = new URLSearchParams({
+          currentJobId: jobId,
+          country: country,
+          refresh: true
+        });
+
+        if (distance !== 'none') {
+          queryParams.append('distance', distance);
+        }
+
+        if (timePosted === 'past24Hours') {
+          queryParams.append('f_TPR', 'r86400');
+        } else if (timePosted === 'pastWeek') {
+          queryParams.append('f_TPR', 'r604800');
+        } else if (timePosted === 'pastMonth') {
+          queryParams.append('f_TPR', 'r2592000');
+        }
+
+        if (remote === 'Yes') {
+          queryParams.append('f_WT', '2');
+        }
+
+        const fullURL = `${baseURL}${queryParams.toString()}`;
+
+        // Navigate to the URL
+        window.location.href = fullURL;
       }
-
-      if (country) {
-        query += `&country=${country}`;
-      }
-
-      if (timePosted === 'past24Hours') {
-        query += `&f_TPR=r86400`;
-      } else if (timePosted === 'pastWeek') {
-        query += `&f_TPR=r604800`;
-      } else if (timePosted === 'pastMonth') {
-        query += `&f_TPR=r2592000`;
-      }
-
-      if (remote === 'Yes') {
-        query += `&f_WT=2`;
-      }
-
-      const fullURL = `${baseURL}${query}`;
-
-      // Navigate to the URL
-      window.location.href = fullURL;
     });
   });
 }
-
-// ... rest of the code
-
 
 // Function to hide the UI overlay
 function hideOverlay() {
