989fbf4a020650fa0c5ffe5bcd592da5939434da
deanOfWalls <deanofwalls@gmail.com>
2023-09-04 02:20:57 -0400

    button now works as a toggle to display or hide the UI

--- content.js
+++ content.js
@@ -1,10 +1,31 @@
+// Variable to store the state of the overlay
+let overlayVisible = false;
+
 // Function to show the UI overlay
 function showOverlay() {
   const overlayHTML = `
     <div id="overlay">
       <label for="zipCode">Zip Code:</label>
-      <input type="text" id="zipCode">
-      <!-- Add other fields here -->
+      <input type="text" id="zipCode"><br>
+      
+      <label for="country">Country:</label>
+      <select id="country">
+        <option value="USA">USA</option>
+        <option value="Canada">Canada</option>
+        <!-- Add more countries here -->
+      </select><br>
+      
+      <label for="timePosted">Time Posted:</label>
+      <select id="timePosted">
+        <option value="anytime">Any time</option>
+        <option value="pastMonth">Past Month</option>
+        <option value="pastWeek">Past Week</option>
+        <option value="past24Hours">Past 24 hours</option>
+      </select><br>
+      
+      <label for="remote">Remote:</label>
+      <input type="checkbox" id="remote"><br>
+      
       <button id="searchButton">Search</button>
     </div>
   `;
@@ -15,14 +36,27 @@ function showOverlay() {
   // Event listener for the "Search" button
   document.getElementById('searchButton').addEventListener('click', function() {
     const zipCode = document.getElementById('zipCode').value;
-    // Capture other fields here
+    const country = document.getElementById('country').value;
+    const timePosted = document.getElementById('timePosted').value;
+    const remote = document.getElementById('remote').checked;
+    
     // Construct LinkedIn URL
+    // TODO: Update this part to include new parameters
     const baseURL = "https://www.linkedin.com/jobs/search/?";
     const query = `currentJobId=${currentJobId}`;
     const fullURL = `${baseURL}${query}`;
+    
     // Navigate to the URL
     window.location.href = fullURL;
   });
+}
+
+// Function to hide the UI overlay
+function hideOverlay() {
+  const overlay = document.getElementById('overlay');
+  if (overlay) {
+    overlay.remove();
+  }
 }
 
 // Function to get the currentJobId from the URL
@@ -38,19 +72,21 @@ button.id = 'overlayButton';
 button.style.backgroundImage = `url(${chrome.runtime.getURL('noBS.png')})`;
 document.body.appendChild(button);
 
-
-
 // Log for debugging
 console.log('Content script running');
 console.log(button);
 console.log(chrome.runtime.getURL('noBS.png'));
-
 
 // Variable to store the currentJobId
 let currentJobId = null;
 
 // Event listener for the overlay button
 button.addEventListener('click', function() {
-  currentJobId = getCurrentJobId();
-  showOverlay();
+  if (overlayVisible) {
+    hideOverlay();
+  } else {
+    currentJobId = getCurrentJobId();
+    showOverlay();
+  }
+  overlayVisible = !overlayVisible;
 });
