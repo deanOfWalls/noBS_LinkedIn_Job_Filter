ec0c03c26f2544f452655d04f4e972ce0498d990
deanOfWalls <deanofwalls@gmail.com>
2023-09-28 02:09:22 -0400

    Consolidated city/state and country fields into a single Location field and added Easy Apply toggle

--- content.js
+++ content.js
@@ -1,38 +1,42 @@
-let overlayVisible = false; // Add this line to define overlayVisible
+let overlayVisible = false;
 
-function buildAndNavigateURL(city, state, country, geoId, keywords, distance, timePosted, remote) {
-  const jobId = getCurrentJobId();
-  const location = `${city}, ${state}, ${country}`;
-  const baseURL = 'https://www.linkedin.com/jobs/search/?';
+function buildAndNavigateURL(city, state, country, geoId, keywords, distance, timePosted, remote, easyApply) {
+    const jobId = getCurrentJobId();
+    const baseURL = 'https://www.linkedin.com/jobs/search/?';
 
-  let urlParams = [
-    `country=${country}`,
-    `currentJobId=${jobId}`,
-    `geoId=${geoId}`,
-    `refresh=true`,
-    `keywords=${encodeURIComponent(keywords)}`,
-    `f_E=2`  // Entry-level flag
-  ];
+    let urlParams = [
+        `country=${country}`,
+        `currentJobId=${jobId}`,
+        `geoId=${geoId || ''}`,
+        `refresh=true`,
+        `keywords=${encodeURIComponent(keywords)}`,
+        `f_E=2`  // Entry-level flag
+    ];
 
-  if (timePosted === 'past24Hours') {
-    urlParams.push(`f_TPR=r86400`);  // Jobs posted within the last 24 hours
-  }
+    if (timePosted === 'past24Hours') {
+        urlParams.push(`f_TPR=r86400`);  // Jobs posted within the last 24 hours
+    }
 
-  if (city.toLowerCase() !== 'none' && state.toLowerCase() !== 'none') {
-    urlParams.push(`location=${encodeURIComponent(location)}`);
-  }
+    if (city && state) {
+        const location = `${city}, ${state}, ${country}`;
+        urlParams.push(`location=${encodeURIComponent(location)}`);
+    }
 
-  if (distance !== 'none') {
-    urlParams.push(`distance=${distance}`);
-  }
+    if (distance !== 'none') {
+        urlParams.push(`distance=${distance}`);
+    }
 
-  const fullURL = `${baseURL}${urlParams.join('&')}`;
-  window.location.href = fullURL;
+    if (easyApply === 'Yes') {
+        urlParams.push(`f_AL=true`);
+    }
+
+    const fullURL = `${baseURL}${urlParams.join('&')}`;
+    window.location.href = fullURL;
 }
 
 function showOverlay() {
-  chrome.storage.sync.get(['country', 'timePosted', 'remote', 'cityStateValue', 'positiveTerms', 'negativeTerms', 'distance'], function (items) {
-    const overlayHTML = `
+    chrome.storage.sync.get(['location', 'timePosted', 'remote', 'positiveTerms', 'negativeTerms', 'distance', 'easyApply'], function (items) {
+        const overlayHTML = `
       <div id="overlay">
         <label for="positiveTerms">Positive Search Terms:</label>
         <input type="text" id="positiveTerms" value="" autocomplete="off"><br>
@@ -43,10 +47,11 @@ function showOverlay() {
           <option value="Senior, Lead, Manager, Director, Principal, Mid, Experienced, 4+, 5+, 6+, 7+, 8+, II, III, IV, V, VI, VII, VIII"></option>
         </datalist>
         
-        <label for="cityState">City/State:</label>
-        <select id="cityState">
-          <option value="New Castle, Delaware, 101877462">New Castle, Delaware</option>
-          <option value="Philadelphia, Pennsylvania, 104937023">Philadelphia, Pennsylvania</option>
+        <label for="location">Location:</label>
+        <select id="location">
+          <option value="New Castle, Delaware, 101877462, USA">New Castle, Delaware, USA</option>
+          <option value="Philadelphia, Pennsylvania, 104937023, USA">Philadelphia, Pennsylvania, USA</option>
+          <option value=", , , USA">USA</option>
         </select><br>
         
         <label for="distance">Distance:</label>
@@ -60,12 +65,6 @@ function showOverlay() {
           <option value="100">100 miles</option>
         </select><br>
         
-        <label for="country">Country:</label>
-        <select id="country">
-          <option value="USA">USA</option>
-          <option value="Canada">Canada</option>
-        </select><br>
-        
         <label for="timePosted">Time Posted:</label>
         <select id="timePosted">
           <option value="anytime">Any time</option>
@@ -76,6 +75,12 @@ function showOverlay() {
         
         <label for="remote">Remote:</label>
         <select id="remote">
+          <option value="No">No</option>
+          <option value="Yes">Yes</option>
+        </select><br>
+
+        <label for="easyApply">Easy Apply:</label>
+        <select id="easyApply">
           <option value="No">No</option>
           <option value="Yes">Yes</option>
         </select><br>
@@ -100,63 +105,58 @@ function showOverlay() {
         <span style="--i:16">o</span>
         <span style="--i:17">m</span>
       </a>
-      
       </div>
     `;
-    const overlay = document.createElement('div');
-    overlay.innerHTML = overlayHTML;
-    document.body.appendChild(overlay);
+        const overlay = document.createElement('div');
+        overlay.innerHTML = overlayHTML;
+        document.body.appendChild(overlay);
 
-    document.getElementById('positiveTerms').value = items.positiveTerms || '';
-    document.getElementById('negativeTerms').value = items.negativeTerms || '';
-    document.getElementById('cityState').value = items.cityState || 'New Castle, Delaware, 101877462';
-    document.getElementById('distance').value = items.distance || 'none';  // Fetch distance here
-    document.getElementById('country').value = items.country || 'USA';
-    document.getElementById('timePosted').value = items.timePosted || 'anytime';
-    document.getElementById('remote').value = items.remote || 'No';
+        document.getElementById('positiveTerms').value = items.positiveTerms || '';
+        document.getElementById('negativeTerms').value = items.negativeTerms || '';
+        document.getElementById('location').value = items.location || 'New Castle, Delaware, 101877462, USA';
+        document.getElementById('distance').value = items.distance || 'none';
+        document.getElementById('timePosted').value = items.timePosted || 'anytime';
+        document.getElementById('remote').value = items.remote || 'No';
+        document.getElementById('easyApply').value = items.easyApply || 'No';
 
-    const searchButton = document.getElementById('searchButton');
-    searchButton.addEventListener('click', function () {     
-   
-  
-      const cityStateElem = document.getElementById('cityState');
-      const cityStateValue = cityStateElem.value;
-      const [city, state, geoId] = cityStateValue.split(', ');
+        const searchButton = document.getElementById('searchButton');
+        searchButton.addEventListener('click', function () {
+            const locationValue = document.getElementById('location').value;
+            const [city, state, geoId, country] = locationValue.split(', ');
 
-      const country = document.getElementById('country').value;
-      const timePosted = document.getElementById('timePosted').value;
-      const remote = document.getElementById('remote').value;
-      const distance = document.getElementById('distance').value;
-      const positiveTerms = document.getElementById('positiveTerms').value;
-      const negativeTerms = document.getElementById('negativeTerms').value;
+            const timePosted = document.getElementById('timePosted').value;
+            const remote = document.getElementById('remote').value;
+            const distance = document.getElementById('distance').value;
+            const positiveTerms = document.getElementById('positiveTerms').value;
+            const negativeTerms = document.getElementById('negativeTerms').value;
+            const easyApply = document.getElementById('easyApply').value;
 
-      // Adjust keywords formatting
-      let keywords = positiveTerms;
-      if (negativeTerms) {
-        const negativeTermsArray = negativeTerms.split(', ').map(term => term.trim());
-        keywords = `${positiveTerms} NOT ${negativeTermsArray.join(' NOT ')}`;
-      }
+            let keywords = positiveTerms;
+            if (negativeTerms) {
+                const negativeTermsArray = negativeTerms.split(', ').map(term => term.trim());
+                keywords = `${positiveTerms} NOT ${negativeTermsArray.join(' NOT ')}`;
+            }
 
-      buildAndNavigateURL(city, state, country, geoId, keywords, distance, timePosted, remote);
-      saveUserSelections(country, timePosted, remote, cityStateValue, positiveTerms, negativeTerms, distance);
-      hideOverlay();
+            buildAndNavigateURL(city, state, country, geoId, keywords, distance, timePosted, remote, easyApply);
+            saveUserSelections(locationValue, timePosted, remote, positiveTerms, negativeTerms, distance, easyApply);
+            hideOverlay();
+        });
     });
-  });
 }
 
 function hideOverlay() {
-  const overlay = document.getElementById('overlay');
-  if (overlay) {
-    overlay.remove();
-    overlayVisible = false;
-  }
+    const overlay = document.getElementById('overlay');
+    if (overlay) {
+        overlay.remove();
+        overlayVisible = false;
+    }
 }
 
 function getCurrentJobId() {
-  const url = new URL(window.location.href);
-  const params = new URLSearchParams(url.search);
-  const jobId = params.get('currentJobId'); // Get the currentJobId from the URL parameter
-  return jobId || '';
+    const url = new URL(window.location.href);
+    const params = new URLSearchParams(url.search);
+    const jobId = params.get('currentJobId');
+    return jobId || '';
 }
 
 const button = document.createElement('button');
@@ -165,44 +165,46 @@ button.style.backgroundImage = `url(${chrome.runtime.getURL('noBS.png')})`;
 document.body.appendChild(button);
 
 button.addEventListener('click', function () {
-  if (overlayVisible) {
-    hideOverlay();
-  } else {
-    showOverlay();
-    
-  }
-  overlayVisible = !overlayVisible;
+    if (overlayVisible) {
+        hideOverlay();
+    } else {
+        showOverlay();
+    }
+    overlayVisible = !overlayVisible;
 });
 
-function saveUserSelections(country, timePosted, remote, cityStateValue, positiveTerms, negativeTerms, distance) {
-  chrome.storage.sync.set({
-    country: country,
-    timePosted: timePosted,
-    remote: remote,
-    cityStateValue: cityStateValue,
-    positiveTerms: positiveTerms,
-    negativeTerms: negativeTerms,
-    distance: distance
-  }, function() {
-    if (chrome.runtime.lastError) {
-      console.error("Error saving distance:", chrome.runtime.lastError);
-    } else {
-      console.log("Distance saved:", distance);
-    }
-  });
+function saveUserSelections(locationValue, timePosted, remote, positiveTerms, negativeTerms, distance, easyApply) {
+    chrome.storage.sync.set({
+        location: locationValue,
+        timePosted: timePosted,
+        remote: remote,
+        positiveTerms: positiveTerms,
+        negativeTerms: negativeTerms,
+        distance: distance,
+        easyApply: easyApply
+    }, function () {
+        if (chrome.runtime.lastError) {
+            console.error("Error saving distance:", chrome.runtime.lastError);
+        } else {
+            console.log("Distance saved:", distance);
+        }
+    });
 }
 
 function loadUserSelections() {
-  if (chrome.storage) {
-    chrome.storage.sync.get(['country', 'timePosted', 'remote', 'cityStateValue', 'positiveTerms', 'negativeTerms'], function (items) {
-      if (document.getElementById('cityState')) {
-        document.getElementById('cityState').value = items.cityStateValue || 'New Castle, Delaware, 101877462';
-      }
-      if (document.getElementById('distance')) {
-        document.getElementById('distance').value = items.distance || 'none';
-      }
-    });
-  } else {
-    console.error('chrome.storage is not available');
-  }
+    if (chrome.storage) {
+        chrome.storage.sync.get(['location', 'timePosted', 'remote', 'positiveTerms', 'negativeTerms', 'distance', 'easyApply'], function (items) {
+            if (document.getElementById('location')) {
+                document.getElementById('location').value = items.location || 'New Castle, Delaware, 101877462, USA';
+            }
+            if (document.getElementById('distance')) {
+                document.getElementById('distance').value = items.distance || 'none';
+            }
+            if (document.getElementById('easyApply')) {
+                document.getElementById('easyApply').value = items.easyApply || 'No';
+            }
+        });
+    } else {
+        console.error('chrome.storage is not available');
+    }
 }
