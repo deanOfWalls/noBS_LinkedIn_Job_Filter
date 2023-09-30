8836fedf04597f530ae9a7df9f52698e634e5884
deanOfWalls <deanofwalls@gmail.com>
2023-09-30 04:53:57 -0400

    Added preset locations for major cities in all states as well as placeholder text for input fields

--- content.js
+++ content.js
@@ -36,7 +36,7 @@ function buildAndNavigateURL(city, state, country, geoId, keywords, distance, ti
 
 function showOverlay() {
   chrome.storage.sync.get(['location', 'timePosted', 'remote', 'positiveTerms', 'negativeTerms', 'distance', 'easyApply', 'mode'], function (items) {
-      const overlayHTML = `
+    const overlayHTML = `
           <div id="overlay">
               <label class="mode-label">Dark/Light Mode</label>
               <label class="switch">
@@ -44,17 +44,163 @@ function showOverlay() {
                   <span class="slider"></span>
               </label>
               <label for="positiveTerms">Positive Search Terms:</label>
-              <input type="text" id="positiveTerms" value="" autocomplete="off"><br>
+              <input type="text" id="positiveTerms" placeholder="E.g., entry level software developer, java" value="" autocomplete="off"><br>
               
               <label for="negativeTerms">Negative Search Terms:</label>
-              <input type="text" id="negativeTerms" value="" autocomplete="off"><br>
+              <input type="text" id="negativeTerms" placeholder="E.g., senior, 5+" value="" autocomplete="off"><br>
              
+
               <label for="location">Location:</label>
               <select id="location">
-                  <option value="New Castle, Delaware, 101877462, USA">New Castle, Delaware, USA</option>
-                  <option value="Philadelphia, Pennsylvania, 104937023, USA">Philadelphia, Pennsylvania, USA</option>
                   <option value=", , , USA">USA</option>
+                  <option value="Birmingham, Alabama, 102905961, USA">Birmingham, Alabama, USA</option>
+                  <option value="Montgomery, Alabama, 105392391, USA">Montgomery, Alabama, USA</option>
+                  <option value="Huntsville, Alabama, 105142920, USA">Huntsville, Alabama, USA</option>
+                  <option value="Anchorage, Alaska, 101830238, USA">Anchorage, Alaska, USA</option>
+                  <option value="Fairbanks, Alaska, 104572344, USA">Fairbanks, Alaska, USA</option>
+                  <option value="Juneau, Alaska, 100852499, USA">Juneau, Alaska, USA</option>
+                  <option value="Phoenix, Arizona, 100219842, USA">Phoenix, Arizona, USA</option>
+                  <option value="Tucson, Arizona, 103752383, USA">Tucson, Arizona, USA</option>
+                  <option value="Mesa, Arizona, 100558512, USA">Mesa, Arizona, USA</option>
+                  <option value="Little Rock, Arkansas, 104302746, USA">Little Rock, Arkansas, USA</option>
+                  <option value="Fayetteville, Arkansas, 105633049, USA">Fayetteville, Arkansas, USA</option>
+                  <option value="Fort Smith, Arkansas, 102509050, USA">Fort Smith, Arkansas, USA</option>
+                  <option value="Los Angeles, California, 102448103, USA">Los Angeles, California, USA</option>
+                  <option value="San Diego, California, 103918656, USA">San Diego, California, USA</option>
+                  <option value="San Jose, California, 106233382, USA">San Jose, California, USA</option>
+                  <option value="San Francisco, California, 102277331, USA">San Francisco, California, USA</option>
+                  <option value="Sacramento, California, 101857797, USA">Sacramento, California, USA</option>
+                  <option value="Denver, Colorado, 103736294, USA">Denver, Colorado, USA</option>
+                  <option value="Colorado Springs, Colorado, 100182490, USA">Colorado Springs, Colorado, USA</option>
+                  <option value="Aurora, Colorado, 106606877, USA">Aurora, Colorado, USA</option>
+                  <option value="Bridgeport, Connecticut, 100199330, USA">Bridgeport, Connecticut, USA</option>
+                  <option value="New Haven, Connecticut, 101911097, USA">New Haven, Connecticut, USA</option>
+                  <option value="Hartford, Connecticut, 101325776, USA">Hartford, Connecticut, USA</option>
+                  <option value="Wilmington, Delaware, 105138576, USA">Wilmington, Delaware, USA</option>
+                  <option value="Jacksonville, Florida, 100868799, USA">Jacksonville, Florida, USA</option>
+                  <option value="Miami, Florida, 102394087, USA">Miami, Florida, USA</option>
+                  <option value="Tampa, Florida, 105517665, USA">Tampa, Florida, USA</option>
+                  <option value="Orlando, Florida, 105142029, USA">Orlando, Florida, USA</option>
+                  <option value="Atlanta, Georgia, 106224388, USA">Atlanta, Georgia, USA</option>
+                  <option value="Augusta, Georgia, 108959117, USA">Augusta, Georgia, USA</option>
+                  <option value="Columbus, Georgia, 102437047, USA">Columbus, Georgia, USA</option>
+                  <option value="Honolulu, Hawaii, 105879727, USA">Honolulu, Hawaii, USA</option>
+                  <option value="Boise, Idaho, 102381687, USA">Boise, Idaho, USA</option>
+                  <option value="Nampa, Idaho, 100748018, USA">Nampa, Idaho, USA</option>
+                  <option value="Chicago, Illinois, 103112676, USA">Chicago, Illinois, USA</option>
+                  <option value="Aurora, Illinois, 104382747, USA">Aurora, Illinois, USA</option>
+                  <option value="Rockford, Illinois, 106717984, USA">Rockford, Illinois, USA</option>
+                  <option value="Indianapolis, Indiana, 100871315, USA">Indianapolis, Indiana, USA</option>
+                  <option value="Fort Wayne, Indiana, 106502857, USA">Fort Wayne, Indiana, USA</option>
+                  <option value="Evansville, Indiana, 103041884, USA">Evansville, Indiana, USA</option>
+                  <option value="Des Moines, Iowa, 105056705, USA">Des Moines, Iowa, USA</option>
+                  <option value="Cedar Rapids, Iowa, 106585497, USA">Cedar Rapids, Iowa, USA</option>
+                  <option value="Davenport, Iowa, 100918937, USA">Davenport, Iowa, USA</option>
+                  <option value="Wichita, Kansas, 100652883, USA">Wichita, Kansas, USA</option>
+                  <option value="Overland Park, Kansas, 104027599, USA">Overland Park, Kansas, USA</option>
+                  <option value="Kansas City, Kansas, 104388316, USA">Kansas City, Kansas, USA</option>
+                  <option value="Louisville, Kentucky, 108449684, USA">Louisville, Kentucky, USA</option>
+                  <option value="Lexington, Kentucky, 114499923, USA">Lexington, Kentucky, USA</option>
+                  <option value="Bowling Green, Kentucky, 107023414, USA">Bowling Green, Kentucky, USA</option>
+                  <option value="New Orleans, Louisiana, 106689237, USA">New Orleans, Louisiana, USA</option>
+                  <option value="Baton Rouge, Louisiana, 105831158, USA">Baton Rouge, Louisiana, USA</option>
+                  <option value="Shreveport, Louisiana, 106758579, USA">Shreveport, Louisiana, USA</option>
+                  <option value="Portland, Maine, 106827813, USA">Portland, Maine, USA</option>
+                  <option value="Lewiston, Maine, 104035811, USA">Lewiston, Maine, USA</option>
+                  <option value="Baltimore, Maryland, 106330734, USA">Baltimore, Maryland, USA</option>
+                  <option value="Frederick, Maryland, 3730211287, USA">Frederick, Maryland, USA</option>
+                  <option value="Rockville, Maryland, 100249151, USA">Rockville, Maryland, USA</option>
+                  <option value="Boston, Massachusetts, 102380872, USA">Boston, Massachusetts, USA</option>
+                  <option value="Worcester, Massachusetts, 104689951, USA">Worcester, Massachusetts, USA</option>
+                  <option value="Springfield, Massachusetts, 100632918, USA">Springfield, Massachusetts, USA</option>
+                  <option value="Detroit, Michigan, 103624908, USA">Detroit, Michigan, USA</option>
+                  <option value="Grand Rapids, Michigan, 100061294, USA">Grand Rapids, Michigan, USA</option>
+                  <option value="Warren, Michigan, 106646796, USA">Warren, Michigan, USA</option>
+                  <option value="Minneapolis, Minnesota, 103039849, USA">Minneapolis, Minnesota, USA</option>
+                  <option value="St Paul, Minnesota, 102370339, USA">Saint Paul, Minnesota, USA</option>
+                  <option value="Rochester, Minnesota, 3730211287, USA">Rochester, Minnesota, USA</option>
+                  <option value="Jackson, Mississippi, 100853037, USA">Jackson, Mississippi, USA</option>
+                  <option value="Gulfport, Mississippi, 100960260, USA">Gulfport, Mississippi, USA</option>
+                  <option value="Biloxi, Mississippi, 103699215, USA">Biloxi, Mississippi, USA</option>
+                  <option value="Kansas City, Missouri, 106142749, USA">Kansas City, Missouri, USA</option>
+                  <option value="St Louis, Missouri, 104428936, USA">Saint Louis, Missouri, USA</option>
+                  <option value="Springfield, Missouri, 104102413, USA">Springfield, Missouri, USA</option>
+                  <option value="Billings, Montana, 104158430, USA">Billings, Montana, USA</option>
+                  <option value="Missoula, Montana, 103023809, USA">Missoula, Montana, USA</option>
+                  <option value="Great Falls, Montana, 105928748, USA">Great Falls, Montana, USA</option>
+                  <option value="Omaha, Nebraska, 100739428, USA">Omaha, Nebraska, USA</option>
+                  <option value="Lincoln, Nebraska, 106469669, USA">Lincoln, Nebraska, USA</option>
+                  <option value="Bellevue, Nebraska, 102457028, USA">Bellevue, Nebraska, USA</option>
+                  <option value="Las Vegas, Nevada, 100293800, USA">Las Vegas, Nevada, USA</option>
+                  <option value="Henderson, Nevada, 104093424, USA">Henderson, Nevada, USA</option>
+                  <option value="Reno, Nevada, 106693758, USA">Reno, Nevada, USA</option>
+                  <option value="Manchester, New Hampshire, 104439903, USA">Manchester, New Hampshire, USA</option>
+                  <option value="Nashua, New Hampshire, 106489956, USA">Nashua, New Hampshire, USA</option>
+                  <option value="Newark, New Jersey, 103913444, USA">Newark, New Jersey, USA</option>
+                  <option value="Jersey City, New Jersey, 102340689, USA">Jersey City, New Jersey, USA</option>
+                  <option value="Paterson, New Jersey, 102390836, USA">Paterson, New Jersey, USA</option>
+                  <option value="Albuquerque, New Mexico, 104055874, USA">Albuquerque, New Mexico, USA</option>
+                  <option value="Las Cruces, New Mexico, 104022305, USA">Las Cruces, New Mexico, USA</option>
+                  <option value="Rio Rancho, New Mexico, 105787306, USA">Rio Rancho, New Mexico, USA</option>
+                  <option value="New York City, New York, 102571732, USA">New York City, New York, USA</option>
+                  <option value="Buffalo, New York, 103676418, USA">Buffalo, New York, USA</option>
+                  <option value="Rochester, New York, 106553046, USA">Rochester, New York, USA</option>
+                  <option value="Charlotte, North Carolina, 102264677, USA">Charlotte, North Carolina, USA</option>
+                  <option value="Raleigh, North Carolina, 100197101, USA">Raleigh, North Carolina, USA</option>
+                  <option value="Greensboro, North Carolina, 106414689, USA">Greensboro, North Carolina, USA</option>
+                  <option value="Fargo, North Dakota, 102300628, USA">Fargo, North Dakota, USA</option>
+                  <option value="Bismarck, North Dakota, 102737163, USA">Bismarck, North Dakota, USA</option>
+                  <option value="Grand Forks, North Dakota, 103985908, USA">Grand Forks, North Dakota, USA</option>
+                  <option value="Columbus, Ohio, 102812094, USA">Columbus, Ohio, USA</option>
+                  <option value="Cleveland, Ohio, 102356711, USA">Cleveland, Ohio, USA</option>
+                  <option value="Cincinnati, Ohio, 106310628, USA">Cincinnati, Ohio, USA</option>
+                  <option value="Oklahoma City, Oklahoma, 101060990, USA">Oklahoma City, Oklahoma, USA</option>
+                  <option value="Tulsa, Oklahoma, 101891230, USA">Tulsa, Oklahoma, USA</option>
+                  <option value="Norman, Oklahoma, 100716176, USA">Norman, Oklahoma, USA</option>
+                  <option value="Portland, Oregon, 104727230, USA">Portland, Oregon, USA</option>
+                  <option value="Salem, Oregon, 102572422, USA">Salem, Oregon, USA</option>
+                  <option value="Eugene, Oregon, 101560118, USA">Eugene, Oregon, USA</option>
+                  <option value="Philadelphia, Pennsylvania, USA">Philadelphia, Pennsylvania, USA</option>
+                  <option value="Pittsburgh, Pennsylvania, 104937023, USA">Pittsburgh, Pennsylvania, USA</option>
+                  <option value="Allentown, Pennsylvania, 106588646, USA">Allentown, Pennsylvania, USA</option>
+                  <option value="Providence, Rhode Island, 106094923, USA">Providence, Rhode Island, USA</option>
+                  <option value="Columbia, South Carolina, 105682819, USA">Columbia, South Carolina, USA</option>
+                  <option value="Charleston, South Carolina, 107200988, USA">Charleston, South Carolina, USA</option>
+                  <option value="Greenville, South Carolina, 102419620, USA">Greenville, South Carolina, USA</option>
+                  <option value="Sioux Falls, South Dakota, 103695844, USA">Sioux Falls, South Dakota, USA</option>
+                  <option value="Rapid City, South Dakota, 101392951, USA">Rapid City, South Dakota, USA</option>
+                  <option value="Aberdeen, South Dakota, 101815058, USA">Aberdeen, South Dakota, USA</option>
+                  <option value="Nashville, Tennessee, 105573479, USA">Nashville, Tennessee, USA</option>
+                  <option value="Memphis, Tennessee, 100420597, USA">Memphis, Tennessee, USA</option>
+                  <option value="Knoxville, Tennessee, 104362759, USA">Knoxville, Tennessee, USA</option>
+                  <option value="Houston, Texas, 103743442, USA">Houston, Texas, USA</option>
+                  <option value="San Antonio, Texas, 102396835, USA">San Antonio, Texas, USA</option>
+                  <option value="Dallas, Texas, 104194190, USA">Dallas, Texas, USA</option>
+                  <option value="Austin, Texas, 104472865, USA">Austin, Texas, USA</option>
+                  <option value="Fort Worth, Texas, 100432370, USA">Fort Worth, Texas, USA</option>
+                  <option value="El Paso, Texas, 102994360, USA">El Paso, Texas, USA</option>
+                  <option value="Salt Lake City, Utah, 106513356, USA">Salt Lake City, Utah, USA</option>
+                  <option value="West Valley City, Utah, 104252509, USA">West Valley City, Utah, USA</option>
+                  <option value="Provo, Utah, 106484743, USA">Provo, Utah, USA</option>
+                  <option value="Burlington, Vermont, 101545250, USA">Burlington, Vermont, USA</option>
+                  <option value="South Burlington, Vermont, 105070696, USA">South Burlington, Vermont, USA</option>
+                  <option value="Virginia Beach, Virginia, 106468467, USA">Virginia Beach, Virginia, USA</option>
+                  <option value="Norfolk, Virginia, 104167482, USA">Norfolk, Virginia, USA</option>
+                  <option value="Chesapeake, Virginia, 106976053, USA">Chesapeake, Virginia, USA</option>
+                  <option value="Seattle, Washington, 104116203, USA">Seattle, Washington, USA</option>
+                  <option value="Spokane, Washington, 106141823, USA">Spokane, Washington, USA</option>
+                  <option value="Tacoma, Washington, 104976816, USA">Tacoma, Washington, USA</option>
+                  <option value="Charleston, West Virginia, 104911336, USA">Charleston, West Virginia, USA</option>
+                  <option value="Huntington, West Virginia, 102784804, USA">Huntington, West Virginia, USA</option>
+                  <option value="Parkersburg, West Virginia, 104363153, USA">Parkersburg, West Virginia, USA</option>
+                  <option value="Milwaukee, Wisconsin, 105240372, USA">Milwaukee, Wisconsin, USA</option>
+                  <option value="Madison, Wisconsin, 106816259, USA">Madison, Wisconsin, USA</option>
+                  <option value="Green Bay, Wisconsin, 105135127, USA">Green Bay, Wisconsin, USA</option>
+                  <option value="Cheyenne, Wyoming, 103451076, USA">Cheyenne, Wyoming, USA</option>
+                  <option value="Casper, Wyoming, 104533488, USA">Casper, Wyoming, USA</option>
+                  <option value="Laramie, Wyoming, 104574744, USA">Laramie, Wyoming, USA</option>
               </select><br>
+              
               
               <label for="distance">Distance:</label>
               <select id="distance">
@@ -88,31 +234,35 @@ function showOverlay() {
               </select><br>
               
               <button id="searchButton">Search</button>
-              <a href="http://www.deanwalls.com" target="_blank" id="deanWallsLink">
-                  <span style="--i:1">w</span>
-                  <span style="--i:2">w</span>
-                  <span style="--i:3">w</span>
-                  <span style="--i:4">.</span>
-                  <span style="--i:5">d</span>
-                  <span style="--i:6">e</span>
-                  <span style="--i:7">a</span>
-                  <span style="--i:8">n</span>
-                  <span style="--i:9">w</span>
+              <a href="http://deanwalls.com" target="_blank" id="deanWallsLink">
+              
+                  <span style="--i:1">h</span>
+                  <span style="--i:2">t</span>
+                  <span style="--i:3">t</span>
+                  <span style="--i:4">p</span>
+                  <span style="--i:5">:</span>
+                  <span style="--i:6">/</span>
+                  <span style="--i:7">/</span>
+                  <span style="--i:8">d</span>
+                  <span style="--i:9">e</span>
                   <span style="--i:10">a</span>
-                  <span style="--i:11">l</span>
-                  <span style="--i:12">l</span>
-                  <span style="--i:13">s</span>
-                  <span style="--i:14">.</span>
-                  <span style="--i:15">c</span>
-                  <span style="--i:16">o</span>
-                  <span style="--i:17">m</span>
+                  <span style="--i:11">n</span>
+                  <span style="--i:12">w</span>
+                  <span style="--i:13">a</span>
+                  <span style="--i:14">l</span>
+                  <span style="--i:15">l</span>
+                  <span style="--i:16">s</span>
+                  <span style="--i:17">.</span>
+                  <span style="--i:18">c</span>
+                  <span style="--i:19">o</span>
+                  <span style="--i:20">m</span>
               </a>
           </div>
       `;
 
-      const overlay = document.createElement('div');
-      overlay.innerHTML = overlayHTML;
-      document.body.appendChild(overlay);
+    const overlay = document.createElement('div');
+    overlay.innerHTML = overlayHTML;
+    document.body.appendChild(overlay);
 
     document.getElementById('positiveTerms').value = items.positiveTerms || '';
     document.getElementById('negativeTerms').value = items.negativeTerms || '';
